ff6d0609078e959591033f9f3935610a2f89bbe5
YIXINlonly <659879286@qq.com>
2022-10-26 13:48:39 +0800

    modify task1

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+int main() {
+	double y = 2;
+	double x = 0;
+	printf("Please enter something\n");
+	
+	printf("       x          y\n");
+
+	
+	while( y -y*0.9990005> 0.000001){
+		x = x+0.000001;
+		y = y*0.9990005;
+		printf("   %f     %f\n",x,y);
+	}
+		
+	printf("The final stationary state is%f\n",y);
 	return 0;
 }
 
